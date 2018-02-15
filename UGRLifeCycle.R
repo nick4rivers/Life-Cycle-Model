884811261639a350b7dff7fbc9f84ca1dd44cc62
Nick Weber <nick@Nicks-MacBook-Pro-2.local>
2018-02-15 14:46:15 -0800

    Working Model No Sup
    This is a full working model, in order to use it at this point I would need to add the hatchery and supplementation scenarios.

--- UGRLifeCycle.R
+++ UGRLifeCycle.R
@@ -1,15 +1,8 @@
-#########################################
-##New
 
-
-##############################################
-###LETS READ IN THE HEADER FILE###############
 
 #Delete Later
 years <- 30
-runs <- 10
-
-
+runs <- 20
 
 #---------------READ IN THE INPUT FILE----------------------#
 input <- read.csv(file.choose(), stringsAsFactors = FALSE)
@@ -24,10 +17,10 @@ for (i in 1:nrow(input)) {
 stages <- unique(input$Stage1)
 
 #add years, reps and any other placeholders needed to our vector
-stages <- c("Year", "Run", stages)
+stages <- c("Year", "Run", stages, "Spawner1Egg", "Spawner2Egg", "Spawner3Egg", "WildSpawners")
 
 #Create a dataframe equal to years and columns needed
-sims <- data.frame(matrix(NaN, nrow=years, ncol=length(stages)))
+sims <- data.frame(matrix(0, nrow=years, ncol=length(stages)))
 
 #Name the columns in our dataframe
 names(sims) <- stages
@@ -40,8 +33,6 @@ names(sims) <- stages
 bev_holt <- function (stage1, stage1_parms) {
   stage1/(((1/rnorm(1, mean = stage1_parms[1], sd = stage1_parms[2]))+((1/rnorm(1, mean = stage1_parms[3], sd = stage1_parms[4]))*stage1)))
 }
-
-
 
 ########################################
 ####     START MODEL LOOPS         #####
@@ -61,40 +52,139 @@ for (j in 1:runs) {
     
     
     ##########################################
-    #####       START AT YEAR 2         ######
+    #####       STARTs AT YEAR 5         #####
     ##########################################
     
+    #IN YEAR
+    #--------------LGD Adult 3 to Trap Adult 3----------------#   
+    if (i > 4) {
+      sims$TrapAdult3[i] <- bev_holt(sims$LGDAdult3[i], p_LGDAdult3_TrapAdult3)
+    }
+    
+    #--------------Trap Adult 3 to Spawner 3----------------#    
+    if (i > 4) {
+      sims$Spawner3[i] <- bev_holt(sims$TrapAdult3[i], p_TrapAdult3_Spawner3)
+    }
+    
+    #--------------Spawner 3 to Spawner Egg 3 ----------------#  
+    if (i > 4 ) {
+      sims$Spawner3Egg[i] <- bev_holt(sims$Spawner3[i], p_Spawner3_Spawner3Egg)
+    }
+    
+    ##########################################
+    #####       STARTs AT YEAR 4         #####
+    ##########################################
+    
+    #IN YEAR
+    #Age 2 adults return to spawn as 2s and produce eggs
+    #--------------LGD Adult 2 to Trap Adult 2----------------#    
+    if (i > 3) {
+      sims$TrapAdult2[i] <- bev_holt(sims$LGDAdult2[i], p_LGDAdult2_TrapAdult2)
+    }
+    
+    #--------------Trap Adult 2 to Spawner 2----------------#    
+    if (i > 3) {
+      sims$Spawner2[i] <- bev_holt(sims$TrapAdult2[i], p_TrapAdult2_Spawner2)
+    }
+    
+    #--------------Spawner 2 to Spawner Egg 2 ----------------#  
+    if (i > 3 ) {
+      sims$Spawner2Egg[i] <- bev_holt(sims$Spawner2[i], p_Spawner2_Spawner2Egg)
+    }
+    
+    #ADD YEAR
+    #--------------Ocean Adult 2 to   LGD Adult 3----------------#
+    if (i > 3 & i < years) {
+      sims$LGDAdult3[i+1] <- bev_holt((sims$OceanAdult2[i] * p_OceanAdult2_LGDAdult3[5]), p_OceanAdult2_LGDAdult3)
+    }
+    
+    ##########################################
+    #####       STARTs AT YEAR 3         #####
+    ##########################################
+    
+    #IN YEAR
+    #--------------LGD Adult 1 to Trap Adult 1----------------#    
+    if (i > 2) {
+     sims$TrapAdult1[i] <- bev_holt(sims$LGDAdult1[i], p_LGDAdult1_TrapAdult1)
+    }
+    
+    #--------------Trap Adult 1  to Spawner 1----------------#    
+    if (i > 2) {
+      sims$Spawner1[i] <- bev_holt(sims$TrapAdult1[i], p_TrapAdult1_Spawner1)
+    }
+    
+    #--------------Spawner 1 to Spawner Egg 1 ----------------#  
+    if (i > 2 ) {
+      sims$Spawner1Egg[i] <- bev_holt(sims$Spawner1[i], p_Spawner1_Spawner1Egg)
+    }
+    
+    #-------------- Sum Eggs ----------------#  
+    if (i > 2 ) {
+      sims$Egg[i] <- sims$Spawner1Egg[i] + sims$Spawner2Egg[i]+ sims$Spawner3Egg[i]
+    }
+    
+    #-------------- Sum Wild Spawners ----------------#  
+    if (i > 2 ) {
+      sims$WildSpawners[i] <- sims$Spawner1[i] + sims$Spawner2[i]+ sims$Spawner3[i]
+    }
+    
+    #ADD YEAR
+    #--------------Ocean Adult 1 to Ocean Adult 2----------------#
+    if (i > 2 & i < years) {
+      sims$OceanAdult2[i+1] <- bev_holt((sims$OceanAdult1[i] * p_OceanAdult1_OceanAdult2[5]), p_OceanAdult1_OceanAdult2)
+    }
+    
+    #--------------Ocean Adult 1 to   LGD Adult 2----------------#
+    if (i > 2 & i < years) {
+      sims$LGDAdult2[i+1] <- bev_holt((sims$OceanAdult1[i] * p_OceanAdult1_LGDAdult2[5]), p_OceanAdult1_LGDAdult2)
+    }
+    
+    #--------------Egg to Fry----------------#
+    if (i > 2 & i < years) {
+      sims$Fry[i+1] <- bev_holt(sims$Egg[i], p_Egg_Fry)
+    }
+  
+    ##########################################
+    #####       STARTs AT YEAR 2         #####
+    ##########################################
+    
+    #IN YEAR
     #--------------Pre Smolts to LGD Smolts----------------#
     #Total smolts at LGD as sum of headwaters and valley pre-smolts
     if (i > 1) {
       sims$LGDSmolt[i] <- 
         (bev_holt(sims$PreSmoltHeadwaters[i], p_PreSmoltHeadwaters_LGDSmolt)
          +
-           bev_holt(sims$PreSmoltValley[i], p_PreSmoltValley_LGDSmolt))
+        bev_holt(sims$PreSmoltValley[i], p_PreSmoltValley_LGDSmolt))
     }
     
-    ###FRY FROM SMOLT TO TEST TO THIS POINT
-    ##This Will end up being LGDsmolts to LGDAge1
-    ####Happens first in Year two, so don't do in year 1#######
-    if (i > 1) {
-      sims$Fry[i] <-  bev_holt(sims$LGDSmolt[i], p_LGDSmolt_LGDAdult1)
+    #ADD YEAR
+    #--------------LGD Smolts to LGD Adults 1----------------#
+    #LGDsmolts * maturation probability
+    if (i > 1 & i < years) {
+      sims$LGDAdult1[i + 1] <-  bev_holt((sims$LGDSmolt[i] * p_LGDSmolt_LGDAdult1[5]), p_LGDSmolt_LGDAdult1)
+    }
+  
+    #--------------LGD Smolts to Ocean Adults 1----------------#
+    #LGDsmolts * stay in ocean probability
+    if (i > 1 & i < years) {
+      sims$OceanAdult1[i + 1] <-  bev_holt((sims$LGDSmolt[i] * p_LGDSmolt_OceanAdult1[5]), p_LGDSmolt_OceanAdult1)
     }
     
-    
     ##########################################
-    #####       START AT YEAR 1         ######
+    #####       STARTs AT YEAR 1         #####
     ##########################################
     
-    
+    #IN YEAR
     #--------------FRY TO PARR----------------#
     sims$Parr[i] <- bev_holt(sims$Fry[i], p_Fry_Parr)
   
-    #--------------PARR TO PRE SMOLTS----------------#
-    #Note no Survival Here - they just move####
     #ADD YEAR
+    #--------------PARR TO PRE SMOLTS----------------#
+    #Note no Survival Here - they just move
     if (i < years) {
-      sims$PreSmoltHeadwaters[i + 1] <- sims$Parr[i] * p_PreSmoltHeadwaters_LGDSmolt[5]
-      sims$PreSmoltValley[i + 1] <- sims$Parr[i] * p_PreSmoltValley_LGDSmolt[5]
+      sims$PreSmoltHeadwaters[i + 1] <- sims$Parr[i] * p_Parr_PreSmoltHeadwaters[5]
+      sims$PreSmoltValley[i + 1] <- sims$Parr[i] * p_Parr_PreSmoltValley[5]
     }
     
     #Print each year iteration
